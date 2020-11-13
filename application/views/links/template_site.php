c35648f16ff543cbf0d5ac565c341aba49ce4673
Jair Filho <Jair Filho@DESKTOP-A9T0FQE>
2020-11-13 16:03:07 -0200

    primeiro teste

--- application/views/links/template_site.php
+++ application/views/links/template_site.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Jekyll v4.0.1">
-    <title>Projeto Final E2</title>
+    <title>Titulo teste</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/carousel/">
 
