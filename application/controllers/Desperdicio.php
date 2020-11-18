99641360834ba574ba54b3678088944ebd328026
ivan- <softip@gmail.com>
2020-11-18 17:29:26 -0200

    Alterado o label do dia para dia e tipo no disperdicio

--- application/controllers/Desperdicio.php
+++ application/controllers/Desperdicio.php
@@ -18,7 +18,7 @@ class Desperdicio extends CI_Controller{
         $crud->set_subject("desperdicio");
            
         
-        $crud->set_relation("refeicao_idrefeicao", "refeicao", "dia");
+        $crud->set_relation("refeicao_idrefeicao", "refeicao", "{dia}-{tipo}");
         $crud->display_as("refeicao_idrefeicao", "Refeição");
         
         $form = $crud->render();
