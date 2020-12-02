ec3d1439b85586e9c4d440888b8bc20482610497
Cliente <Cliente@DESKTOP-D19QFI3>
2020-12-02 16:46:30 -0200

    Mudamos algumas coisas nos gráficos

--- application/views/template/adminlte/admin.php
+++ application/views/template/adminlte/admin.php
@@ -94,7 +94,6 @@
         <?php endforeach; ?>
             
         <!-- Data Table -->
- <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
 	<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
 	<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
 	<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
